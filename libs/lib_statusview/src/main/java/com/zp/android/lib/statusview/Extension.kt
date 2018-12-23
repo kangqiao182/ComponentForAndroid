48ff8470cae08af8f64ff1cd519788251c4a05af
kangqiao <kangqiao610@gmail.com>
2018-12-23 22:42:27 +0800

    增加StatusView 扩展.

--- libs/lib_statusview/src/main/java/com/zp/android/lib/statusview/Extension.kt
+++ libs/lib_statusview/src/main/java/com/zp/android/lib/statusview/Extension.kt
@@ -1,6 +1,19 @@
 package com.zp.android.lib.statusview
 
+import android.app.Activity
+import android.support.annotation.IdRes
+import android.support.annotation.LayoutRes
+import android.support.v4.app.Fragment
+
 /**
  * Created by zhaopan on 2018/12/6.
  */
 
+inline fun Activity.initStatusView() = StatusView.init(this)
+inline fun Activity.initStatusView(@IdRes id: Int) = StatusView.init(this, id)
+inline fun Fragment.initStatusView(@IdRes id: Int) = StatusView.init(this, id)
+
+inline fun StatusView.setStatusView(index: Int, @LayoutRes layoutId: Int, listener: StatusViewConvertListener) {
+    this.setStatusView(index, layoutId)
+    this.setOnStatusViewConvertListener(index, listener)
+}
